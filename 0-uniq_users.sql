b4d9d78ef92f9326ea04e13a2117c9146b8626fd
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-08-08 12:08:55 -0400

    Healing.

--- 0-uniq_users.sql
+++
@@ -1,8 +0,0 @@
--- This SQL script creates a table users that has
--- these attributes: id, email, name
-
-CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
-);
--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- This ranks country origins of bands, 
 -- ordered by the number of (non-unique) fans
-SELECT origin, SUM(fans) AS Lfans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
-ORDER BY Lfans DESC;
+ORDER BY nb_fans DESC;
