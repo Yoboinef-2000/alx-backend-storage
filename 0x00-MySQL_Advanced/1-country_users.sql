5382789e786b8521047588f67bd7913b2d4bb2b0
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-08-08 12:22:21 -0400

    Darling.

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,6 @@
 -- This SQL script creates a table users that has
 -- these attributes: id, email, name, country
-
+DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
