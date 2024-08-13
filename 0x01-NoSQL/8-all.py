920f412c540b459f6059972f55f10fa456a9b7d8
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-08-13 04:35:05 -0400

    Backs broken.

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""Python function that lists all documents in a collection."""
+from pymongo import MongoClient
+
+
+def list_all(mongo_collection):
+    """List all documents in a collection."""
+    return list(mongo_collection.find())
